56b65aea3c1ea6b3e67593dca4f482b32924ba93
U-BSD\411915 <411915@SHS-HJ86ND2.beaverton.k12.or.us>
2022-12-12 14:21:32 -0800

    third

--- zuul.cpp
+++ zuul.cpp
@@ -14,11 +14,27 @@
 using namespace std;
 
 void createRooms(vector<Room*>* rooms);
+void printRooms(vector<Room*>* rooms, int currentRoom);
 
 int main() {
 	bool play = true;
+	char command[10]; 
+
 	vector<Room*> rooms;
+	
 	createRooms(&rooms);
+	int currentRoom = 1;
+
+	cout << "" << endl;
+
+	while (play == true) {
+		cout << "You are in the ";
+		printRooms(&rooms, currentRoom);
+		cin.get(command, 10);
+		cin.get();
+
+
+	}
 
 	return 0;
 }
@@ -116,8 +132,105 @@ void createRooms(vector<Room*>* rooms) {
         rooms->push_back(kitchen);
         tempMap.clear();
 
+	//pantry
+        Room* pantry= new Room();
+        pantry->setTrait((char*)("watermelon flavored dumplings."));
+        pantry->setID(7);
+        tempMap.insert(pair<int, char*>(8, west)); //wine room
+        tempMap.insert(pair<int, char*>(6, south)); //kitchen
+        pantry->setExits(tempMap);
+        rooms->push_back(pantry);
+        tempMap.clear();
+	
+	//wine room
+        Room* wineroom = new Room();
+        wineroom->setTrait((char*)("can you smell what the rock is cooking."));
+        wineroom->setID(8);
+        tempMap.insert(pair<int, char*>(7, east)); //pantry
+        wineroom->setExits(tempMap);
+        rooms->push_back(wineroom);
+        tempMap.clear();
+
+	//elevator
+        Room* elevator = new Room();
+        elevator->setTrait((char*)("can you smell what the rock is cooking."));
+        elevator->setID(9);
+        tempMap.insert(pair<int, char*>(3, west)); //garden
+        elevator->setExits(tempMap);
+        rooms->push_back(elevator);
+        tempMap.clear();
+	
+	//balconey room
+        Room* balconeyroom = new Room();
+        balconeyroom->setTrait((char*)("can you smell what the rock is cooking."));
+        balconeyroom->setID(10);
+        tempMap.insert(pair<int, char*>(3, south)); //garden
+        balconeyroom->setExits(tempMap);
+        rooms->push_back(balconeyroom);
+        tempMap.clear();
+	
+	//bedroom
+        Room* bedroom = new Room();
+        bedroom->setTrait((char*)("can you smell what the rock is cooking."));
+        bedroom->setID(11);
+        tempMap.insert(pair<int, char*>(13, west)); //game room
+        tempMap.insert(pair<int, char*>(12, south)); //living room
+        bedroom->setExits(tempMap);
+        rooms->push_back(bedroom);
+        tempMap.clear();
+
+	//living room
+        Room* livingroom = new Room();
+        livingroom->setTrait((char*)("can you smell what the rock is cooking."));
+        livingroom->setID(12);
+        tempMap.insert(pair<int, char*>(11, north)); //bedroom
+        tempMap.insert(pair<int, char*>(4, south)); //bathroom
+	tempMap.insert(pair<int, char*>(3, east)); //garden
+        livingroom->setExits(tempMap);
+        rooms->push_back(livingroom);
+        tempMap.clear();
+
+	//gameroom
+        Room* gameroom = new Room();
+        gameroom->setTrait((char*)("can you smell what the rock is cooking."));
+        gameroom->setID(13);
+        tempMap.insert(pair<int, char*>(11, east)); //bedroom
+        tempMap.insert(pair<int, char*>(14, south)); //work room
+        gameroom->setExits(tempMap);
+        rooms->push_back(gameroom);
+        tempMap.clear();
+
+	//work room
+        Room* workroom = new Room();
+        workroom->setTrait((char*)("can you smell what the rock is cooking."));
+        workroom->setID(14);
+        tempMap.insert(pair<int, char*>(13, north)); //gameroom
+        workroom->setExits(tempMap);
+        rooms->push_back(workroom);
+        tempMap.clear();
+
+	//pool
+        Room* pool = new Room();
+        pool->setTrait((char*)("can you smell what the rock is cooking."));
+        pool->setID(6);
+        tempMap.insert(pair<int, char*>(4, east)); //bathroom
+        pool->setExits(tempMap);
+        rooms->push_back(pool);
+        tempMap.clear();
 
 
 }
 
-
+void printRooms(vector<Room*>* rooms, int currentRoom) {
+	vector<Room*>::iterator room;
+	for (room = rooms->begin(); room != rooms->end();room++) {
+		if (currentRoom == (*room)->getID()) {
+			cout << (*room)->getTrait() << endl;
+			cout << "Exits: ";
+			for (map<int, char*>::const_iterator r = (*room) -> getExits()->begin();r != (*room) -> getExits() -> end(); r++) {
+				cout << r->second << " ";
+			}
+			cout << endl;
+		}
+	}
+}
